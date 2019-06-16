d5c7eaa066f988b13e1ce1ab8d0df036f48becdf
Julien AMREIN <github.julien@gmail.com>
2019-06-16 23:24:02 +0200

    Update I2C_Scanner_LCD_Arduino.ino
    Code from setup() to loop()

--- I2C_Scanner_LCD_Arduino/I2C_Scanner_LCD_Arduino.ino
+++ I2C_Scanner_LCD_Arduino/I2C_Scanner_LCD_Arduino.ino
@@ -6,7 +6,13 @@ void setup()
     Serial.begin(9600);
     while (!Serial);
     
-    byte codeRetour = 9;
+    
+  }
+
+void loop()
+  {
+  
+  byte codeRetour = 9;
     byte adresse = 1;
   
     for(adresse = 1; adresse < 127; adresse++)
@@ -23,9 +29,5 @@ void setup()
           Serial.print("\n");
         }
       }
-  }
-
-void loop()
-  {
-  
+  delay(10000)
   }
